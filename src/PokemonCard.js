e0beb3d74e6357509a87242a54ce754333e7c34a
unknown <jreidmke@gmail.com>
2020-12-10 09:41:30 -0600

    imported custom hook useFlip

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import "./PokemonCard.css";
+import useFlip from "./hooks/useFlip";
 
 /* Renders a single pokemon card. */
 function PokemonCard({ front, back, name, stats }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  // const [showing, setshowing] = useState(true);
+  // const setShowing = () => {
+  //   setshowing(isUp => !isUp);
+  // };
+
+  const [showing, setShowing] = useFlip();
   return (
-    <div onClick={flipCard} className="PokemonCard Card">
-      {isFacingUp ? (
+    <div onClick={setShowing} className="PokemonCard Card">
+      {showing ? (
         <div className="PokemonCard-front">
           <img src={front} alt={`{name} front`} />
           <div>
